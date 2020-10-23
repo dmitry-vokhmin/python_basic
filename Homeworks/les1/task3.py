a13cfb2850fe3ee795a8a31dc3e7bd70314820f5
Zarus <boxdima1@gmail.com>
2020-10-23 10:03:58 -0400

    Homework is done

---
+++ Homeworks/les1/task3.py
@@ -0,0 +1,5 @@
+user_number = int(input("Введите любое число >>>"))
+number1 = str(user_number) + str(user_number)
+number2 = number1 + str(user_number)
+print(user_number + int(number1) + int(number2))
+
