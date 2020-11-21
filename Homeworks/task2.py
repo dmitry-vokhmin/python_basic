5c7c6dabc0c5aa95bc0e4a0e742f695629b726d2
Zarus <boxdima1@gmail.com>
2020-11-21 11:08:42 -0500

    update 1.0

--- Homeworks/task2.py
+++ Homeworks/task2.py
@@ -1,26 +1,20 @@
-number = 18
+number = 15
 count = 1
-block = 0
 param = 1
 floor = 0
-if number == 1:
-    floor = 1
-    position = 1
-else:
-    while block == 0:
-        if number in range(1, (count**2 + param)):
-            block = count
-            floor += 1
-        else:
-            param = count**2 + param
-            floor += count
-            count += 1
-    if (number - (param - 1)) % block == 0:
-        floor = floor + ((number - (param - 1)) // block) - 1
-        position = ((number - param) % block) + 1
-    else:
-        floor = floor + ((number - (param - 1)) // block)
-        position = (number - (param - 1)) % block
 
+while number not in range(1, (count**2 + param)):
+        param = count**2 + param
+        floor += count
+        count += 1
+if (number - (param - 1)) % count == 0:
+    floor = floor + ((number - (param - 1)) // count)
+    position = ((number - param) % count) + 1
+else:
+    floor = floor + ((number - (param - 1)) // count) + 1
+    position = (number - (param - 1)) % count
+
+print(param)
+print(count)
 print(f"{floor} этаж")
 print(f"{position} позиция")
